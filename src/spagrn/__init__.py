d132a7b3dc9bcdb08981176c9517b7a43080b569
Lingxiao-LIU <linliu@augusta.edu>
2025-09-09 18:42:13 -0400

    Update __init__.py

--- src/spagrn/__init__.py
+++ src/spagrn/__init__.py
@@ -1,11 +1,45 @@
-# spatialGRN
-# __package__ = 'spagrn'
+# /Users/linliu/NewSpaGRN/src/spagrn/__init__.py
+__package__ = 'spagrn'
 
-__all__ = ['regulatory_network', 'plot', 'network']
+__all__ = [
+    'regulatory_network',
+    'hotspot',
+    'knn',
+    'local_stats',
+    'autocor',
+    'corexp',
+    'c_autocor',
+    'm_autocor',
+    'g_autocor',
+    'network',
+    'danb_model',
+    'bernoulli_model',
+    'normal_model',
+    'none_model',
+    'utils',
+    'plot'
+]
 
-# deprecated to keep older scripts who import this from breaking
+from . import regulatory_network
+from . import hotspot
+from . import knn
+from . import local_stats
+from . import autocor
+from . import corexp
+from . import c_autocor
+from . import m_autocor
+from . import g_autocor
+from . import network
+from . import danb_model
+from . import bernoulli_model
+from . import normal_model
+from . import none_model
+from . import utils
+from . import plot
+
+# Deprecated imports to maintain compatibility with older scripts
 from .regulatory_network import InferNetwork
-from .network import Network, regulon_specificity_scores
+from .network import Network
 
 COLORS = [
     '#d60000', '#e2afaf', '#018700', '#a17569', '#e6a500', '#004b00',
